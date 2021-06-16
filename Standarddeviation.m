64a536d09c0f57edc68122c713d98a2f21b98041
luciedrd <75130940+luciedrd@users.noreply.github.com>
2021-06-16 14:50:00 +0200

    Update and rename EcartTypeVitesse.m to Standarddeviation.m

--- Standarddeviation.m
+++ Standarddeviation.m
@@ -2,7 +2,7 @@ clear;
 close all;
 
 %%
-T1 = readtable('posture  yeux ouverts_Raphaël  Varane   23May21_16_47_58.csv');
+T1 = readtable('posture  yeux ouverts_RaphaÃ«l  Varane   23May21_16_47_58.csv');
 PL=str2double(table2array(T1(2:750,2:5)));
 PR=str2double(table2array(T1(2:750,7:10)));
 
@@ -36,26 +36,32 @@ for i=1:length(PR)
  
 end
 
+%% Calculation of SD for Right side
 
-speedR=[];
-ecartTypeR=0;
+frequency=25;
+meanSpeed=3,2;
+
+speedR=[]; 
+varianceR=0;
 for i=1:length(PR)-1 
     dXR=XR(i+1)-XR(i);
     dYR=YR(i+1)-YR(i);
-    speedR(i)=sqrt(dXR*dXR+dYR*dYR)/0.04;
-    ecartTypeR=ecartTypeR+((speedR(i))-3.2);
+    speedR(i)=sqrt(dXR*dXR+dYR*dYR)/(1/frequency);% calculation of the speed between 2 adjacent points
+    varianceR=varianceR+((speedR(i))-meanSpeed);% calculation of variance : difference between "local" speed and mean speed
 end
 
+%% Same calculation for Left Side
+
 speedL=[];
-ecartTypeL=0;
+varianceL=0;
 for i=1:length(PR)-1 
     dXL=XL(i+1)-XL(i);
     dYL=YL(i+1)-YL(i);
-    speedL(i)=sqrt(dXL*dXL+dYL*dYL)/0.04;
-    ecartTypeL=ecartTypeL+(speedL(i)-3.2);
+    speedL(i)=sqrt(dXL*dXL+dYL*dYL)/(1/frequency);% calculation of the speed between 2 adjacent points
+    varianceL=varianceL+(speedL(i)-meanSpeed);% calculation of variance : difference between "local" speed and mean speed
 end
 
-ecartTypeR = sqrt(ecartTypeR/length(PR)-1);
-ecartTypeL = sqrt(ecartTypeL/length(PL)-1);
+standardDeviationR = sqrt(varianceR/length(PR)-1); 
+standardDeviationL = sqrt(varianceL/length(PL)-1);
 
-ecartType=(ecartTypeR+ecartTypeL)/2;
+standardDeviation=(standardDeviationR+standardDeviationR)/2;
